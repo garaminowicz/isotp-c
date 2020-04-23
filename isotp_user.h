a72b23992d52ffaa1f82ed745176a8271065ef2f
Grzegorz Araminowicz <g.araminowicz@gmail.com>
2020-04-23 11:18:18 +0200

    improve documentation and whitespacing

--- isotp_user.h
+++ isotp_user.h
@@ -5,8 +5,8 @@
 void isotp_user_debug(const char* message, ...);
 
 /* user implemented, send can message */
-int  isotp_user_send_can(const uint32_t arbitration_id,
-                         const uint8_t* data, const uint8_t size);
+int isotp_user_send_can(const uint32_t arbitration_id,
+                        const uint8_t* data, const uint8_t size);
 
 /* user implemented, get millisecond */
 uint32_t isotp_user_get_ms(void);
